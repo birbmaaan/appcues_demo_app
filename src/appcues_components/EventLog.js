302df61b2f12d5c3a9dc1f2a552554568d5edcc3
birdmaaan <elijah.b.dove@gmail.com>
2021-11-16 21:47:04 -0800

    add additional event log comments

--- src/appcues_components/EventLog.js
+++ src/appcues_components/EventLog.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 function EventLog() {
-  const clear_log = () => {
+  const clear_log = () => { // removes all listed events in the event log
     const events_log = document.getElementById('events_log');
     while (events_log.firstChild) {
       events_log.removeChild(events_log.firstChild);
     }
   }
 
-  {
-    return (
-      <div>
-            <button onClick={clear_log}>Clear All Events</button>
-            <h2>See all events here</h2>
-            <ul id="events_log">Flow events</ul>
-        </div>
-    )
-  }
+  
+  return (
+    <div>
+      <button onClick={clear_log}>Clear All Events</button>
+      <h2>See all events here</h2>
+      <ul id="events_log">Flow events</ul>
+    </div>
+  )
+
 }
 
 function log_events(name, payload) { // function to log Appcues events on the page
@@ -33,15 +33,7 @@ function log_events(name, payload) { // function to log Appcues events on the pa
   events_log.appendChild(event_data); // add the payload onto the page
 }
 
-// const parse_data = (data) => { // take a stringified object and format it into readable HTML
-
-// }
-
 export {
   log_events,
   EventLog,
 }
-
-// {"id":"checklist_item_started","name":"Checklist Item Started","checklistId":"-MmPoj9sZviFmgs2CcWK",
-// "checklistName":"Testing checklist 5","itemId":"-MmPoj9t-OX-bsAvImiH","itemIndex":0,"itemLabel":"",
-// "timestamp":1637127349571,"sessionId":1637127347605}
